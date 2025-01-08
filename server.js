9b8b5a621f4c3a7399c0206c3a287316fab07539
highlamjay <caoduonglam61@gmail.com>
2025-01-08 16:14:08 +0700

    first commit

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const dotenv = require('dotenv');
 const mongoose = require('mongoose');
-const routes = require('./routes/');  
+const routes = require('./routes');  
 
 dotenv.config();
 
