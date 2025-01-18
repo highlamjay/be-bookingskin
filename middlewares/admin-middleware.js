70a57618edc20fc9d999afc3d7abcc3b4b252d1d
highlamjay <caoduonglam61@gmail.com>
2025-01-18 23:20:02 +0700

    fix: amdin middleware

--- middlewares/admin-middleware.js
+++ middlewares/admin-middleware.js
@@ -1,9 +1,10 @@
+const jwt = require('jsonwebtoken');
 
 const isAdminUser = (req, res, next) => {
     try {
         const authHeader = req.headers['authorization'];
         const token = authHeader && authHeader.split(' ')[1];
-        
+
         if (!token) {
             return res.status(401).json({
                 success: false,
