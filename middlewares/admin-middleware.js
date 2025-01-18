493c39f604661d5985aba9c15608a2701b34591c
highlamjay <caoduonglam61@gmail.com>
2025-01-18 22:47:05 +0700

    fix middle admin

--- middlewares/admin-middleware.js
+++ middlewares/admin-middleware.js
@@ -1,12 +1,23 @@
 
 const isAdminUser = (req, res, next) => {
     try {
-        if (req.user.role !== 'admin') {
-            return res.status(403).json({ 
-                success: true,
-                message: 'Access denied ! Admin rights required' 
-            })  
-        } 
+        const authHeader = req.headers['authorization'];
+        const token = authHeader && authHeader.split(' ')[1];
+        
+        if (!token) {
+            return res.status(401).json({
+                success: false,
+                message: "Unauthorized! No token provided.",
+            });
+        }
+
+        const decoded = jwt.verify(token, process.env.JWT_SECRET); // Giải mã token
+        if (decoded.role !== "admin") {
+            return res.status(403).json({
+                success: false,
+                message: "Access denied! Admin rights required.",
+            });
+        }
         next();
     } catch (error) {
         console.log(error);
--- routes/product-route.js
+++ routes/product-route.js
@@ -11,7 +11,7 @@ const {
 } = require('../controllers/product-controller'); 
 const adminMiddleware = require('../middlewares/admin-middleware');
 
-router.post('/create', adminMiddleware, uploadMiddleware.fields([{ name: "image" }, { name: "video" }]) ,createProduct)
+router.post('/create', adminMiddleware, uploadMiddleware.fields([{ name: "image" }, { name: "video" }]) , createProduct)
 router.get('/fetch-all', fetchAllProducts)
 router.get('/fetch-detail/:id', fetchDetailProduct)
 router.put('/edit/:id', adminMiddleware, editProduct)
